75ffe2dff07ee07cc136a89e94121a23f3a6c88a
zhouchen <1583741285@qq.com>
2019-10-13 16:09:13 +0800

    finish 2019/10/13 task

---
+++ 每天的 code笔记/2019.10.13/flat.js
@@ -0,0 +1,17 @@
+function flattenDeep(input) {
+    const stack = [...input];
+    const res = [];
+    while (stack.length) {
+        // 使用 pop 从 stack 中取出并移除值
+        const next = stack.pop();
+        if (Array.isArray(next)) {
+            // 使用 push 送回内层数组中的元素，不会改动原始输入 original input
+            stack.push(...next);
+        } else {
+            res.push(next);
+        }
+    }
+    // 使用 reverse 恢复原数组的顺序
+    return res.reverse();
+}
+console.log(flattenDeep([1, [2, [3, [4]], 5]]));
