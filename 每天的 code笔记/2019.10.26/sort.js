1857bf933e3b31637ec5a7fca3e9939213389b8c
zhouchen <1583741285@qq.com>
2019-10-26 16:41:03 +0800

    add 2019.10.26 task

---
+++ 每天的 code笔记/2019.10.26/sort.js
@@ -0,0 +1,14 @@
+var obj = {
+    '2': 3,
+    '3': 4,
+    'length': 2,
+    'splice': Array.prototype.splice,
+    'push': Array.prototype.push
+}
+console.log(obj)
+obj.push(1)
+console.log(obj)
+obj.push(2)
+console.log(obj)
+obj.push(3)
+console.log(obj)
